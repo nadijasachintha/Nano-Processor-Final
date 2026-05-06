98a57d784d5204b7c56032314b2df9c09d7f676d
Lakshan <lakshan15352@gmail.com>
2026-05-06 17:33:00 +0530

    add D_FF component

--- components/D_FF/D_FF.vhd
+++ components/D_FF/D_FF.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 05/06/2026 05:25:31 PM
--- Design Name: 
--- Module Name: D_FF - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
